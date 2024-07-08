f076cf5a3d7f1b9cfb228cb9b362387eeaa66e66
hinda <hinda40311@gmail.com>
2024-07-08 13:26:50 +0300

    add Request Status Screen

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,8 +3,9 @@ import { createStore } from "redux";
 import { combineReducers } from "redux";
 import { reportsReducer } from "./reducer/reportsReducer";
 import { activityLogReducer } from "./reducer/activityLogReducer";
+import { itemReducer } from "./reducer/itemReducer";
 
-export const reducer=combineReducers({userReducer,reportsReducer,activityLogReducer})
+export const reducer=combineReducers({userReducer,reportsReducer,activityLogReducer,itemReducer})
 
 
 
