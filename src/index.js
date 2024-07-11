dd302d2e4e5e28f99e20562486175d7f06406b30
tsiporaShub <t8506671@gmail.com>
2024-07-11 13:21:26 +0300

    fix the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
-import { reducer, store } from './redux/store';
+import './index.css';
 import App from './App';
 import theme from './theme';
 import reportWebVitals from './reportWebVitals';
@@ -19,5 +19,7 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
