4be3f9cdb0d609875b574a552862b70ab61537d7
AyalaKelerman <149535284+AyalaKelerman@users.noreply.github.com>
2024-08-01 09:54:12 +0300

    Update Roting.jsx

--- src/components/Roting.jsx
+++ src/components/Roting.jsx
@@ -25,7 +25,7 @@ export const Routing = () => {
      
       <Routes>
         <Route path="/StatusListView" element={<StatusListView />} />
-        <Route path="/SearchAppBar" element={<AllSearchScreen />} />
+        <Route path="/search" element={<AllSearchScreen />} />
         <Route path="/SavedItemsScreen" element={<SavedItemsScreen />} />
         <Route path='/ActivityLog' element={<ExternalRedirect url="https://login.foirstein.diversitech.co.il/#/ActivityLog" />} />
         <Route path='/Charts' element={<ExternalRedirect url="https://login.foirstein.diversitech.co.il/#/Charts" />} />
